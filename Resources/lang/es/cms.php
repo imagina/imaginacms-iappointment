8308f2487a0126001dc794b6d8377e91a578ea14
Michael <msolano@imaginacolombia.com>
2022-02-11 19:09:00 -0500

    +Update: cms Translation files

--- Resources/lang/es/cms.php
+++ Resources/lang/es/cms.php
@@ -1,34 +1,43 @@
 <?php
+
 return [
-  "newAppointment" => 'Nueva Cita',
-  "updateAppointment" => 'Actualizar Cita',
-  "newCategory" => 'Nueva Categoría',
-  "updateCategory" => 'Actualizar Categoría',
-  "post" => 'Entrada',
-  "quickCard" => [
-    "lastPosts" => 'Blog - Ultimos Posts'
+  "sidebar" => [
+    "adminGroup" => "Citas",
+    "adminAppointment" => "Lista de citas",
+    "adminCategories" => "Categorias de citas",
+    "adminStatus" => "Lista de estados",
+    "panelAppointments" => "Lista de Citas"
   ],
-  "form "=> [
-    "showInMenu" => 'Mostrar en menu',
-    "customer" => 'Cliente',
-    "assigned" => 'Asignado a',
-    "newStatus" => 'Nuevo estado'
+  "newAppointment" => "Nueva Cita",
+  "updateAppointment" => "Actualizar Cita",
+  "newCategory" => "Nueva Categoría",
+  "updateCategory" => "Actualizar Categoría",
+  "post" => "Entrada",
+  "quickCard" => [
+    "lastPosts" => "Blog - Ultimos Posts"
+  ],
+  "form" => [
+    "showInMenu" => "Mostrar en menu",
+    "customer" => "Cliente",
+    "assigned" => "Asignado a",
+    "newStatus" => "Nuevo estado"
   ],
   "message" => [
-    "assigningMessage" => 'Estamos asignando un profesional para tu cita, espera por favor...',
-    "expiredMessage" => 'Tu cita ha expirado...',
-    "completedMessage" => 'Tu cita ha terminado...',
-    "abandonedMessage" => 'Tu cita fue abandonada...',
-    "appointmentList" => 'Lista de Citas',
-    "readFullConversation" => 'Leer la conversación completa',
-    "readRecomendations" => 'Leer recomendaciones',
-    "goToAppointment" => 'Ir a la cita',
-    "addRecommendation "=> 'Agregar Recomendación',
-    "changeStatus" => 'Cambiar Estado',
-    "showInfo" => 'Ver Información',
-    "resumAppointment" => 'Retomar cita',
-    "newAssignationMessage" => 'Tienes una nueva assignación!',
-    "reportError" => 'Reportar un Error',
-    "showSubscriptions"  => 'Ver Suscripciones'
+    "assigningMessage" => "Estamos asignando un profesional para tu cita, espera por favor...",
+    "expiredMessage" => "Tu cita ha expirado...",
+    "completedMessage" => "Tu cita ha terminado...",
+    "abandonedMessage" => "Tu cita fue abandonada...",
+    "appointmentList" => "Lista de Citas",
+    "readFullConversation" => "Leer la conversación completa",
+    "readRecomendations" => "Leer recomendaciones",
+    "goToAppointment" => "Ir a la cita",
+    "addRecommendation" => "Agregar Recomendación",
+    "changeStatus" => "Cambiar Estado",
+    "showInfo" => "Ver Información",
+    "resumAppointment" => "Retomar cita",
+    "newAssignationMessage" => "Tienes una nueva assignación!",
+    "reportError" => "Reportar un Error",
+    "showSubscriptions" => "Ver Suscripciones"
   ]
 ];
+
