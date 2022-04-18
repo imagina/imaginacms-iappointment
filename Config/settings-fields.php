05dcdfc7a73c9da1934f2a1ad0766e3266bf633c
Cristian Marin <marin101998@gmail.com>
2022-04-18 15:11:45 -0500

    + update | settings fields with params only super admin

--- Config/settings-fields.php
+++ Config/settings-fields.php
@@ -1,82 +1,84 @@
 <?php
 
 return [
-    'roleToCustomer' => [
-        'name' => 'iappointment::roleToCustomer',
-        'value' => null,
-        'type' => 'select',
-        'props' => [
-            'label' => 'iappointment::settings.roleAsCustomer',
-            'useChips' => true
-        ],
-        'loadOptions' => [
-            'apiRoute' => 'apiRoutes.quser.roles',
-            'select' => ['label' => 'name', 'id' => 'id']
-        ]
+  'roleToCustomer' => [
+    'name' => 'iappointment::roleToCustomer',
+    "onlySuperAdmin" => true,
+    'value' => null,
+    'type' => 'select',
+    'props' => [
+      'label' => 'iappointment::settings.roleAsCustomer',
+      'useChips' => true
     ],
-    'roleToAssigned' => [
-        'name' => 'iappointment::roleToAssigned',
-        'value' => null,
-        'type' => 'select',
-        'props' => [
-            'label' => 'iappointment::settings.roleAsAssigned',
-            'useChips' => true,
-            'multiple' => true,
-        ],
-        'loadOptions' => [
-            'apiRoute' => 'apiRoutes.quser.roles',
-            'select' => ['label' => 'name', 'id' => 'id']
-        ]
+    'loadOptions' => [
+      'apiRoute' => 'apiRoutes.quser.roles',
+      'select' => ['label' => 'name', 'id' => 'id']
+    ]
+  ],
+  'roleToAssigned' => [
+    'name' => 'iappointment::roleToAssigned',
+    "onlySuperAdmin" => true,
+    'value' => null,
+    'type' => 'select',
+    'props' => [
+      'label' => 'iappointment::settings.roleAsAssigned',
+      'useChips' => true,
+      'multiple' => true,
     ],
-    'categoryIndexTitle' => [
-        'name' => 'iappointment::categoryIndexTitle',
-        'value' => '',
-        'type' => 'input',
-        'props' => [
-            'type' => 'text',
-            'label' => 'iappointment::settings.categoryIndexTitle',
-        ],
+    'loadOptions' => [
+      'apiRoute' => 'apiRoutes.quser.roles',
+      'select' => ['label' => 'name', 'id' => 'id']
+    ]
+  ],
+  'categoryIndexTitle' => [
+    'name' => 'iappointment::categoryIndexTitle',
+    'value' => '',
+    'type' => 'input',
+    'props' => [
+      'type' => 'text',
+      'label' => 'iappointment::settings.categoryIndexTitle',
     ],
-    'enableChat' => [
-        'name' => 'iappointment::enableChat',
-        'value' => '0',
-        'type' => 'checkbox',
-        'props' => [
-            'label' => 'iappointment::settings.enableChat',
-            'trueValue' => '1',
-            'falseValue' => '0'
-        ],
+  ],
+  'enableChat' => [
+    'name' => 'iappointment::enableChat',
+    'value' => '0',
+    'type' => 'checkbox',
+    'props' => [
+      'label' => 'iappointment::settings.enableChat',
+      'trueValue' => '1',
+      'falseValue' => '0'
     ],
-    'enableShifts' => [
-        'name' => 'iappointment::enableShifts',
-        'value' => '0',
-        'type' => 'checkbox',
-        'props' => [
-            'label' => 'iappointment::settings.enableShifts',
-            'trueValue' => '1',
-            'falseValue' => '0'
-        ],
+  ],
+  'enableShifts' => [
+    'name' => 'iappointment::enableShifts',
+    'value' => '0',
+    'type' => 'checkbox',
+    'props' => [
+      'label' => 'iappointment::settings.enableShifts',
+      'trueValue' => '1',
+      'falseValue' => '0'
     ],
-    'maxAppointments' => [
-        'name' => 'iappointment::maxAppointments',
-        'value' => '1',
-        'type' => 'input',
-        'props' => [
-            'label' => 'iappointment::settings.maxAppointments',
-            'type' => 'number',
-            'min' => '1',
-        ],
+  ],
+  'maxAppointments' => [
+    'name' => 'iappointment::maxAppointments',
+    'value' => '1',
+    'type' => 'input',
+    'props' => [
+      'label' => 'iappointment::settings.maxAppointments',
+      'type' => 'number',
+      'min' => '1',
     ],
-    'appointmentDayLimit' => [
-        'name' => 'iappointment::appointmentDayLimit',
-        'value' => null,
-        'type' => 'input',
-        'props' => [
-            'label' => 'iappointment::settings.appointmentDayLimit',
-            'type' => 'number',
-            'min' => '1'
-        ],
+  ],
+  'appointmentDayLimit' => [
+    'name' => 'iappointment::appointmentDayLimit',
+    'value' => null,
+    'type' => 'input',
+    'props' => [
+      'label' => 'iappointment::settings.appointmentDayLimit',
+      'type' => 'number',
+      'min' => '1'
     ],
+  ],
 
   'errorFormRelated' => [
     'name' => 'iappointment::errorFormRelated',
