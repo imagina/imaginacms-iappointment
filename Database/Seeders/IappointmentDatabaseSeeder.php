718b57838b851b368ede684c85af2735d29a9a22
Cristian Marin <marin101998@gmail.com>
2023-04-26 12:16:56 -0500

    +add | modules seeds by jobs

--- Database/Seeders/IappointmentDatabaseSeeder.php
+++ Database/Seeders/IappointmentDatabaseSeeder.php
@@ -4,19 +4,21 @@ namespace Modules\Iappointment\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Modules\Isite\Jobs\ProcessSeeds;
 
 class IappointmentDatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        Model::unguard();
-
-        $this->call(IappointmentModuleTableSeeder::class);
-        $this->call(AppointmentStatusTableSeeder::class);
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    Model::unguard();
+    ProcessSeeds::dispatch([
+      "baseClass" => "\Modules\Iappointment\Database\Seeders",
+      "seeds" => ["IappointmentModuleTableSeeder", "AppointmentStatusTableSeeder"]
+    ]);
+  }
 }
